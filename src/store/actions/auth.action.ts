cecdd353b06577a9d191fd130ac4392697795a36
napster125 <napster0125@gmail.com>
2022-02-24 23:49:18 +0500

    Feat : setup redux

---
+++ src/store/actions/auth.action.ts
@@ -0,0 +1,5 @@
+import { AuthActionTypes, AuthAction } from '../../interfaces/store/auth.store.types';
+
+const authLoginStart = (): AuthAction => ({
+    type: AuthActionTypes.AUTH_LOGIN_START,
+});
