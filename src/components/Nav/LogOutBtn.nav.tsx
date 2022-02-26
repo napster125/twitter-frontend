709f440becdbd43380e57bd45b616e97025ad24a
napster125 <napster0125@gmail.com>
2022-02-26 20:08:06 +0500

    Feat : added logout action on btn

--- src/components/Nav/LogOutBtn.nav.tsx
+++ src/components/Nav/LogOutBtn.nav.tsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { authLogout } from '../../store/actions/auth.action';
+import { useDispatch } from 'react-redux';
 
 interface Iprops {
 	displayInDropDown?: boolean;
 }
 
-const LogOutBtn = ({displayInDropDown} : Iprops) => {
-  return (
+const LogOutBtn = ({ displayInDropDown }: Iprops) => {
+	const dispatch = useDispatch();
+	return (
 		<div>
 			<button
 				className={` btn bg-secondary w-90 ${
 					displayInDropDown ? 'w-100 py-2 rounded-0 mt-1' : 'd-lg-flex d-none py-3'
 				} justify-content-center align-items-center me-xl-2 btn-lg   me-1`}
+				onClick={() => dispatch(authLogout())}
 			>
 				<i className='fa-solid fa-arrow-right-from-bracket mt-1 me-xl-3  fs-18'></i>
 				<span className='mt-1 fs-17 d-xl-inline d-none'>Log Out</span>
 			</button>
 		</div>
 	);
-}
+};
 
-export default LogOutBtn
+export default LogOutBtn;
