44f75ec4ddcbbe02023e8e30f27fcec94c84da2e
napster125 <napster0125@gmail.com>
2022-02-28 03:44:25 +0500

    Feat : created a action for getProfileUser

---
+++ src/store/actions/profileInfo.action.ts
@@ -0,0 +1,34 @@
+import {
+	ProfileUserActionTypes,
+	ProfileUserAction,
+} from '../../interfaces/store/profileInfo.types';
+import axios from '../../common/axios';
+import { toast } from 'react-toastify';
+
+
+const getProfileUserStart = (): ProfileUserAction => ({
+    type: ProfileUserActionTypes.GET_PROFILE_USER_START,
+});
+
+const getProfileUserSuccess = (profileUser: any): ProfileUserAction => ({
+    type: ProfileUserActionTypes.GET_PROFILE_USER_SUCCESS,
+    payload: profileUser,
+});
+
+const getProfileUserFailure = (error: any): ProfileUserAction => ({
+    type: ProfileUserActionTypes.GET_PROFILE_USER_FAILURE,
+    payload: error,
+});
+
+
+export const getProfileUser = (userId: string) => async (dispatch: any) => {
+    dispatch(getProfileUserStart());
+    try {
+        const response = await axios.get(`/users/getUserById/${userId}`);
+        const data = await response.data;
+        dispatch(getProfileUserSuccess(data.user));
+    } catch (error:any) {
+        dispatch(getProfileUserFailure(error?.response.data.message));
+        toast.error(error?.response.data.message);
+    }
+};
