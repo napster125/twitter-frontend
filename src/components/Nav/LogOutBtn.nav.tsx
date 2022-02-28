0165d3bb2b3e5418557557b9ec0c990659441a2c
napster125 <napster0125@gmail.com>
2022-02-28 18:37:40 +0500

    Refector : navigate user to home page when logout

--- src/components/Nav/LogOutBtn.nav.tsx
+++ src/components/Nav/LogOutBtn.nav.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import { userLogout } from '../../store/actions/user.action';
 import { useDispatch } from 'react-redux';
 
+import { useNavigate } from 'react-router-dom';
+ 
 interface Iprops {
 	displayInDropDown?: boolean;
 }
 
 const LogOutBtn = ({ displayInDropDown }: Iprops) => {
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	
+	const handleLogOut = () => {
+		dispatch(userLogout());
+		navigate('/');
+	}
+
 	return (
 		<div>
 			<button
 				className={` btn bg-secondary w-90 ${
 					displayInDropDown ? 'w-100 py-2 rounded-0 mt-1' : 'd-lg-flex d-none py-3'
 				} justify-content-center align-items-center me-xl-2 btn-lg   me-1`}
-				onClick={() => dispatch(userLogout())}
+				onClick={handleLogOut}
 			>
 				<i className='fa-solid fa-arrow-right-from-bracket mt-1 me-xl-3  fs-18'></i>
 				<span className='mt-1 fs-17 d-xl-inline d-none'>Log Out</span>
