848b6a6837ae7f392d7f59619947be521d4e069f
napster125 <napster0125@gmail.com>
2022-03-20 19:12:13 +0500

    Feat : created action for  markAsReadNotification

--- src/components/notifications/NotificationListItem.tsx
+++ src/components/notifications/NotificationListItem.tsx
@@ -4,33 +4,43 @@ import { Link } from 'react-router-dom';
 import Avatar from '../common/Avatar';
 import FollowUnfollowBtn from '../sidebar/suggestedUsers/FollowUnfollowBtn';
 import NotificationTypeMessage from './NotificationTypeMessage';
+import { markAsReadNotification } from "../../store/actions/notification.action"
+import { useDispatch } from "react-redux"
 
 interface Iprops {
 	notification: any
 }
 
 const NotificationListItem = ({ notification } : Iprops ) => {
+
+	const dispatch = useDispatch()
+	const handleMarkAsReadNotification = () => dispatch(markAsReadNotification(notification._id))
+
 	return (
 		<li
 			className={`${
 				!notification.isRead && 'bg-secondary bg-opacity-25 rounded-0 '
 			} list-group-item p-4 border-0 border-bottom `}
+			onClick={handleMarkAsReadNotification}
 		>
 			<Link
-				to={`${
-					notification?.tweet
-						? `/tweet/${notification?.tweet._id}`
-						: ``
-				}`}
+				to={`${notification?.tweet ? `/tweet/${notification?.tweet._id}` : ``}`}
 				className={`d-flex justify-content-between`}
 			>
 				<div className={`d-flex w-100 ${!notification?.tweet && 'align-items-center'}`}>
-					<Avatar avatar={notification?.sender.avatar} />
+					{
+						<Link to={`/profile/${notification?.sender._id}`}>
+							<Avatar avatar={notification?.sender.avatar} />
+						</Link>
+					}
 					<section className='mb-0 w-100'>
 						<div className='d-flex mt-1 align-items-center'>
-							<p className='me-9px mb-0 fw-bold text-capitalize'>
+							<Link
+								to={`/profile/${notification?.sender._id}`}
+								className='me-9px mb-0 fw-bold text-capitalize'
+							>
 								{notification?.sender.name}
-							</p>
+							</Link>
 							<NotificationTypeMessage notification={notification} />
 							<span className='fs-13 text-dark text-opacity-75 ms-3'>
 								{moment(notification.createdAt).fromNow()}
@@ -55,9 +65,7 @@ const NotificationListItem = ({ notification } : Iprops ) => {
 							</div>
 						)}
 					</section>
-					{notification.type === 'follow' && (
-						<FollowUnfollowBtn user={notification.sender}/>
-					)}
+					{notification.type === 'follow' && <FollowUnfollowBtn user={notification.sender} />}
 				</div>
 			</Link>
 		</li>
