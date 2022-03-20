08236824ff77e4895ab10002d59810e1c1815ce1
napster125 <napster0125@gmail.com>
2022-03-20 15:47:35 +0500

    Fix : add key to items

--- src/utils/string.utils.tsx
+++ src/utils/string.utils.tsx
@@ -8,11 +8,11 @@ const stringUtils = {
 			.map((word: string) => word.slice(1));
 	},
 	heighLightTrends: (text: string) => {
-		return text.split(' ').map((word: string) => {
+		return text.split(' ').map((word: string,index:number) => {
 			if (word.startsWith('#') && word.length > 1) {
-				return <Link to={`/trends/${word.slice(1)}`} className="text-primary">{ word }</Link>;
+				return <Link key={index} to={`/trends/${word.slice(1)}`} className="text-primary">{ word }</Link>;
 			}
-			return <> {word} </>;
+			return <span key={index}> {word} </span>;
 		})
 	}
 };
