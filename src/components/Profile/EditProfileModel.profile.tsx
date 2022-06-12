5e42f06f7f0050bc91562a0ee333769355120857
napster125 <napster0125@gmail.com>
2022-06-12 14:12:23 +0500

    update

--- src/components/Profile/EditProfileModel.profile.tsx
+++ src/components/Profile/EditProfileModel.profile.tsx
@@ -1,105 +1,116 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-import updateProfileJson from '../../JSON/updateProfileForm.json';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import updateProfileJson from '../../JSON/updateProfileForm.json'
 import {
-	updateUser, updateUserStart, uploadAvatar
-} from '../../store/actions/updateUser.action';
-import Calendar from '../common/Calendar';
-import Spinner from '../common/Spinner';
-import EditProfileCover from './EditProfileCover.profile';
-import { Button, Modal } from 'react-bootstrap';
+	updateUser,
+	updateUserStart,
+	uploadAvatar,
+} from '../../store/actions/updateUser.action'
+import Calendar from '../common/Calendar'
+import Spinner from '../common/Spinner'
+import EditProfileCover from './EditProfileCover.profile'
+import { Button, Modal } from 'react-bootstrap'
 
 interface Iprops {
-	user: any;
-	handleClose: any;
+	user: any
+	handleClose: any
 	show: boolean
 }
 
 const EditProfileModel = ({ user, handleClose, show }: Iprops) => {
-	const dispatch = useDispatch();
-	const { loading, isUserUpdated } = useSelector((state: any) => state.updateUser);
+	const dispatch = useDispatch()
+	const { loading, isUserUpdated } = useSelector(
+		(state: any) => state.updateUser
+	)
 
 	const [date_Of_birth, setDate_Of_birth] = React.useState<any>({
 		month: null,
 		day: null,
 		year: null,
-	});
+	})
 	const handleDate_Of_birth = (date: object) => {
-		setDate_Of_birth(date);
-	};
+		setDate_Of_birth(date)
+	}
 
-	const [avatar, setAvatar] = React.useState<any>(null);
-	const [cover, setCover] = React.useState<any>(null);
+	const [avatar, setAvatar] = React.useState<any>(null)
+	const [cover, setCover] = React.useState<any>(null)
 	const handleAvatarAndCover = ({ state, file }: any) => {
 		if (state === 'avatar') {
-			setAvatar(file);
+			setAvatar(file)
 		} else if (state === 'cover') {
-			setCover(file);
+			setCover(file)
 		}
-	};
+	}
 
 	const [inputFormData, setInputFormData] = React.useState<any>({
 		name: user.name || '',
 		location: user.location || '',
 		website: user.website || '',
 		bio: user.bio || '',
-	});
+	})
 
 	const handleSubmit = async () => {
 		const data: any = {
 			...inputFormData,
-		};
+		}
 
 		const date_Of_birth_is_empty = Object.values(date_Of_birth).some(
-			(value) => value !== null,
-		);
-		const all_fields_are_empty = Object.values(date_Of_birth).every((value) => value !== null);
+			(value) => value !== null
+		)
+		const all_fields_are_empty = Object.values(date_Of_birth).every(
+			(value) => value !== null
+		)
 
 		if (date_Of_birth_is_empty) {
 			if (!all_fields_are_empty) {
-				toast.error('Please fill all fields of date of birth');
-				return;
+				toast.error('Please fill all fields of date of birth')
+				return
 			} else {
-				data.date_Of_birth = `${date_Of_birth.month}/${date_Of_birth.day}/${date_Of_birth.year}`;
+				data.date_Of_birth = `${date_Of_birth.month}/${date_Of_birth.day}/${date_Of_birth.year}`
 			}
 		}
 
-		dispatch(updateUserStart());
+		dispatch(updateUserStart())
 
 		if (avatar) {
-			const avatarSrcUrl = await uploadAvatar(avatar);
-			avatarSrcUrl && (data.avatar = avatarSrcUrl.src);
-			setAvatar(null);
+			const avatarSrcUrl = await uploadAvatar(avatar)
+			avatarSrcUrl && (data.avatar = avatarSrcUrl.src)
+			setAvatar(null)
 		}
 
 		if (cover) {
-			const coverSrcUrl = await uploadAvatar(cover);
-			coverSrcUrl && (data.cover = coverSrcUrl.src);
-			setCover(null);
+			const coverSrcUrl = await uploadAvatar(cover)
+			coverSrcUrl && (data.cover = coverSrcUrl.src)
+			setCover(null)
 		}
 
-		dispatch(updateUser(data));
-	};
+		dispatch(updateUser(data))
+	}
 
 	React.useEffect(() => {
 		if (isUserUpdated) {
-			handleClose();
+			handleClose()
 		}
-	} , [isUserUpdated]);
+	}, [isUserUpdated])
 
 	return (
 		<Modal show={show} onHide={handleClose}>
 			<Modal.Header closeButton>
-				<Modal.Title>Modal heading</Modal.Title>
+				<Modal.Title>Edit Profile</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				<EditProfileCover handleAvatar={handleAvatarAndCover} user={user} />
+				<EditProfileCover
+					handleAvatar={handleAvatarAndCover}
+					user={user}
+				/>
 				{updateProfileJson &&
 					updateProfileJson.map((item: any, index: number) => {
 						return (
 							<div className='mb-3 mt-4' key={index}>
-								<label htmlFor={item.name} className='form-label'>
+								<label
+									htmlFor={item.name}
+									className='form-label'>
 									{item.placeholder}
 								</label>
 								{item.type == 'textarea' ? (
@@ -112,9 +123,8 @@ const EditProfileModel = ({ user, handleClose, show }: Iprops) => {
 											setInputFormData({
 												...inputFormData,
 												[item.name]: e.target.value,
-											});
-										}}
-									></textarea>
+											})
+										}}></textarea>
 								) : (
 									<input
 										type={item.type}
@@ -125,12 +135,12 @@ const EditProfileModel = ({ user, handleClose, show }: Iprops) => {
 											setInputFormData({
 												...inputFormData,
 												[item.name]: e.target.value,
-											});
+											})
 										}}
 									/>
 								)}
 							</div>
-						);
+						)
 					})}
 
 				<div className='mb-3'>
@@ -144,12 +154,15 @@ const EditProfileModel = ({ user, handleClose, show }: Iprops) => {
 				<Button variant='secondary' onClick={handleClose}>
 					Close
 				</Button>
-				<Button variant='dark' onClick={handleSubmit}  disabled={loading}>
+				<Button
+					variant='dark'
+					onClick={handleSubmit}
+					disabled={loading}>
 					{loading ? <Spinner size='sm' /> : 'Save changes'}
 				</Button>
 			</Modal.Footer>
 		</Modal>
-	);
-};
+	)
+}
 
-export default EditProfileModel;
+export default EditProfileModel
