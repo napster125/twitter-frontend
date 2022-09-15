dc70ad8f437a60e05e512a262c64b0dfe30012da
napster125 <napster0125@gmail.com>
2022-09-15 23:38:33 +0500

    Refactor Profile

--- src/pages/secure/Profile.tsx
+++ src/pages/secure/Profile.tsx
@@ -1,43 +1,48 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
-import Nav from '../../components/Profile/Nav.profile';
-import ProfileCover from '../../components/Profile/ProfileCover.profile';
-import ProfileInfo from '../../components/Profile/ProfileInfo.profile';
-import { getProfileUser } from '../../store/actions/profileInfo.action';
-import { Outlet } from 'react-router-dom';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router'
+import TweetNavigation from '../../components/Profile/TweetNavigation'
+import ProfileCover from '../../components/Profile/ProfileCover.profile'
+import UserInformation from '../../components/Profile/UserInformation.profile'
+import { getProfileUser } from '../../store/actions/profileInfo.action'
+import { Outlet } from 'react-router-dom'
 
 const Profile = (props: any) => {
-	const params = useParams();
-	const { id }: any = params;
+	const dispatch = useDispatch()
+	const params = useParams()
+	const { id }: any = params
 
-	const dispatch = useDispatch();
-	const { profileUser, loading, error } = useSelector((state: any) => state.profileUser);
-	const { currentUser } = useSelector((state: any) => state.user);
+	const { profileUser, loading, error } = useSelector(
+		(state: any) => state.profileUser
+	)
+	const { currentUser } = useSelector((state: any) => state.user)
 
 	React.useEffect(() => {
-		dispatch(getProfileUser(id));
-	}, [id]);
+		dispatch(getProfileUser(id))
+	}, [id])
 
 	if (loading) {
-		return <div className='vh-50 center'>Loading...</div>;
+		return <div className='vh-50 center'>Loading...</div>
 	}
 	if (error) {
-		return <div className='vh-50 center'>{error}</div>;
+		return <div className='vh-50 center'>{error}</div>
 	}
 
 	return (
 		profileUser && (
 			<div className='px-md-4'>
-				<ProfileCover currentUser={currentUser} profileUser={profileUser} />
-				<ProfileInfo user={profileUser} />
-				<Nav id={id} />
+				<ProfileCover
+					currentUser={currentUser}
+					profileUser={profileUser}
+				/>
+				<UserInformation user={profileUser} />
+				<TweetNavigation id={id} />
 				<div className='mt-4'>
 					<Outlet />
 				</div>
 			</div>
 		)
-	);
-};
+	)
+}
 
-export default Profile;
+export default Profile
