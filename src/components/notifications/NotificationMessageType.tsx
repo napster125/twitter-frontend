511825a8f0306eaddf83c37f839c528550001d00
napster125 <napster0125@gmail.com>
2022-09-16 14:00:17 +0500

    Refactor code

---
+++ src/components/notifications/NotificationMessageType.tsx
@@ -0,0 +1,17 @@
+interface Iprops {
+	notification: any
+}
+
+const NotificationMessageType = ({ notification }: Iprops) => {
+	return (
+		<div className='mb-0 fs-14 me-3'>
+			{notification.type === 'like' && 'liked your post'}
+			{notification.type === 'comment' && 'commented on your post'}
+			{notification.type === 'follow' && 'followed you'}
+			{notification.type === 'mention' && 'mentioned you in a post '}
+			{notification.type === 'retweet' && 'retweeted your post'}
+		</div>
+	)
+}
+
+export default NotificationMessageType
