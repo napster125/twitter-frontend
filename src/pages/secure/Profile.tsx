41ef53629b07b4a5bfdeca3520b952a8462885f6
napster125 <napster0125@gmail.com>
2022-09-28 13:56:37 +0500

    Refactor

--- src/pages/secure/Profile.tsx
+++ src/pages/secure/Profile.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
+import { Outlet } from 'react-router-dom'
 import NavigationsTweet from '../../components/Profile/NavigationsTweet'
 import ProfileCover from '../../components/Profile/ProfileCover.profile'
 import UserInformation from '../../components/Profile/UserInformation.profile'
 import { getProfileUser } from '../../store/actions/profileInfo.action'
-import { Outlet } from 'react-router-dom'
 import { IRootState } from '../../types/store/IRootState.types'
 
-const Profile = (props: any) => {
+const Profile = () => {
 	const dispatch = useDispatch()
 	const params = useParams()
 	const { id }: any = params
@@ -22,27 +22,22 @@ const Profile = (props: any) => {
 		dispatch(getProfileUser(id))
 	}, [id])
 
-	if (loading) {
-		return <div className='vh-50 center'>Loading...</div>
-	}
-	if (error) {
+	if (loading || !profileUser) return <div>Loading...</div>
+	if (error || !profileUser)
 		return <div className='vh-50 center'>{error}</div>
-	}
 
 	return (
-		profileUser && (
-			<div className='px-md-4'>
-				<ProfileCover
-					currentUser={currentUser}
-					profileUser={profileUser}
-				/>
-				<UserInformation user={profileUser} />
-				<NavigationsTweet id={id} />
-				<div className='mt-4'>
-					<Outlet />
-				</div>
+		<div className='px-md-4'>
+			<ProfileCover
+				currentUser={currentUser}
+				profileUser={profileUser}
+			/>
+			<UserInformation user={profileUser} />
+			<NavigationsTweet id={id} />
+			<div className='mt-4'>
+				<Outlet />
 			</div>
-		)
+		</div>
 	)
 }
 
